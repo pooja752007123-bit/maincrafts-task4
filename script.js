9bfe539133aea7a299112b43141b1b118a8074f4
pooja752007123-bit <pooja752007123@gmail.com>
2026-07-07 21:39:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+function displayTasks(list = tasks) {
+
+    const taskList = document.getElementById("taskList");
+
+    taskList.innerHTML = "";
+
+    list.forEach((task, index) => {
+
+        taskList.innerHTML += `
+        <li>
+            ${task}
+
+            <button onclick="editTask(${index})">Edit</button>
+
+            <button onclick="deleteTask(${index})">Delete</button>
+        </li>
+        `;
+    });
+}
+
+function addTask() {
+
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") {
+
+        alert("Please enter a task");
+        return;
+    }
+
+    tasks.push(input.value);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    input.value = "";
+
+    displayTasks();
+}
+
+function deleteTask(index) {
+
+    tasks.splice(index, 1);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTasks();
+}
+
+function editTask(index) {
+
+    let newTask = prompt("Edit Task", tasks[index]);
+
+    if (newTask !== null && newTask.trim() !== "") {
+
+        tasks[index] = newTask;
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        displayTasks();
+    }
+}
+
+function searchTasks() {
+
+    const search = document
+        .getElementById("searchBox")
+        .value
+        .toLowerCase();
+
+    const filtered = tasks.filter(task =>
+        task.toLowerCase().includes(search)
+    );
+
+    displayTasks(filtered);
+}
+
+displayTasks();
